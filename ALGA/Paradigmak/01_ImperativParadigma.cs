7a2de289ac343ca66f7f7138cde1ce841c3121a1
Obence04 <49245665+Obence04@users.noreply.github.com>
2025-09-21 15:29:09 +0200

    1., 2. labor áthelyezése a megfelelő helyre

--- ALGA/Paradigmak/01_ImperativParadigma.cs
+++ ALGA/Paradigmak/01_ImperativParadigma.cs
@@ -4,6 +4,126 @@ using System.Collections.Generic;
 
 namespace OE.ALGA.Paradigmak
 {
-    // 1. heti labor feladat - Tesztek: 01_ImperativParadigmaTesztek.cs
+    public interface IVegrehajthato
+    {
+        void Vegrehajtas();
+    }
+    public class FeladatTarolo<T> : IEnumerable<T> where T : IVegrehajthato
+    {
+        protected T[] tarolo;
+        protected int n;
+        public FeladatTarolo(int meret)
+        {
+            tarolo = new T[meret];
+            n = 0;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return BejaroLetrehozas();
+        }
+        public void Felvesz(T elem)
+        {
+            if (n < tarolo.Length)
+            {
+                tarolo[n] = elem;
+                n++;
+            }
+            else throw new TaroloMegteltKivetel();
+        }
+        public void MindentVegrehajt()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                tarolo[i].Vegrehajtas();
+            }
+        }
+
+        public IEnumerator<T> BejaroLetrehozas()
+        {
+            FeladatTaroloBejaro<T> ftb = new FeladatTaroloBejaro<T>(tarolo, n);
+            return ftb;
+        }
+
+    }
+
+    public interface IFuggo
+    {
+        public bool FuggosegTeljesul { get; }
+    }
+
+    public class FuggoFeladatTarolo<T> : FeladatTarolo<T> where T : IVegrehajthato, IFuggo
+    {
+
+        public FuggoFeladatTarolo(int meret) : base(meret)
+        {
+
+        }
+        public void MindentVegrehajt()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (tarolo[i].FuggosegTeljesul)
+                {
+                    tarolo[i].Vegrehajtas();
+                }
+            }
+        }
+    }
+
+    public interface IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator();
+    }
+
+    public interface IEnumerator<T>
+    {
+        public T Current { get; }
+        public void Alaphelyzet();
+        public bool MoveNext();
+
+    }
+
+    public class FeladatTaroloBejaro<T> : IEnumerator<T>
+    {
+        T[] tarolo;
+        int n;
+        int aktualisIndex;
+        public T Current { get { return tarolo[aktualisIndex]; } }
+        public FeladatTaroloBejaro(T[] tarolo, int n)
+        {
+            this.tarolo = tarolo;
+            this.n = n;
+            Alaphelyzet();
+        }
+        public void Alaphelyzet()
+        {
+            aktualisIndex = -1;
+        }
+        public bool MoveNext()
+        {
+            aktualisIndex++;
+            if (aktualisIndex < n)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    [Serializable]
+    public class TaroloMegteltKivetel : Exception
+    {
+        public TaroloMegteltKivetel()
+        {
+        }
+
+        public TaroloMegteltKivetel(string? message) : base(message)
+        {
+        }
+
+        public TaroloMegteltKivetel(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
 }
 
--- ALGA/Paradigmak/02_FunkcionalisParadigma.cs
+++ ALGA/Paradigmak/02_FunkcionalisParadigma.cs
@@ -4,5 +4,78 @@ using System.Collections.Generic;
 
 namespace OE.ALGA.Paradigmak
 {
-    // 2. heti labor feladat - Tesztek: 02_FunkcionálisParadigmaTesztek.cs
+    public class FeltetelesFeladatTarolo<T> : FeladatTarolo<T>, IEnumerable<T> where T : IVegrehajthato
+    {
+        public Func<T, bool> BejaroFeltetel { get; set; }
+        public FeltetelesFeladatTarolo(int meret) : base(meret)
+        {
+        }
+        public void FeltetelesVegrehajtas(Func<T, bool> feltetel)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (feltetel(tarolo[i]))
+                {
+                    tarolo[i].Vegrehajtas();
+                }
+            }
+        }
+        public void MindentVegrehajt()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                tarolo[i].Vegrehajtas();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return BejaroLetrehozas();
+        }
+        public FeltetelesFeladatTaroloBejaro<T> BejaroLetrehozas()
+        {
+            if (BejaroFeltetel is null)
+            {
+                FeltetelesFeladatTaroloBejaro<T> fftb1 = new FeltetelesFeladatTaroloBejaro<T>(tarolo, n, x => true);
+                return fftb1;
+            }
+            FeltetelesFeladatTaroloBejaro<T> fftb2 = new FeltetelesFeladatTaroloBejaro<T>(tarolo, n, BejaroFeltetel);
+            return fftb2;
+        }
+    }
+
+    public class FeltetelesFeladatTaroloBejaro<T> : IEnumerator<T>
+    {
+        T[] tarolo;
+        int n;
+        int aktualisIndex;
+        Func<T, bool> feltetel;
+        public T Current { get { return tarolo[aktualisIndex]; } }
+        public FeltetelesFeladatTaroloBejaro(T[] tarolo, int n, Func<T, bool> feltetel)
+        {
+            this.tarolo = tarolo;
+            this.n = n;
+            Alaphelyzet();
+            this.feltetel = feltetel;
+        }
+        public void Alaphelyzet()
+        {
+            aktualisIndex = -1;
+        }
+
+        public bool MoveNext()
+        {
+            do
+            {
+                aktualisIndex++;
+            }
+            while (aktualisIndex < n && !feltetel(tarolo[aktualisIndex]));
+
+            if (aktualisIndex < n)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
 }
