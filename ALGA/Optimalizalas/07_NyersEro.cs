e2dc5d94e768edd442615e482751a5d80016a125
Obence04 <49245665+Obence04@users.noreply.github.com>
2025-10-31 08:23:15 +0100

    7. labor kész nagyon sok gond volt

--- ALGA/Optimalizalas/07_NyersEro.cs
+++ ALGA/Optimalizalas/07_NyersEro.cs
@@ -18,8 +18,9 @@ namespace OE.ALGA.Optimalizalas
         }
         public int OsszSuly(bool[] pakolas)
         {
+            int min = Math.Min(pakolas.Length, w.Length);
             int s = 0;
-            for (int i = 0; i < pakolas.Length; i++)
+            for (int i = 0; i < min; i++)
             {
                 if (pakolas[i]) s += w[i];
             }
@@ -27,8 +28,9 @@ namespace OE.ALGA.Optimalizalas
         }
         public float OsszErtek(bool[] pakolas)
         {
+            int min = Math.Min(pakolas.Length, p.Length);
             float s = 0;
-            for (int i = 0; i < pakolas.Length; i++)
+            for (int i = 0; i < min; i++)
             {
                 if (pakolas[i]) s += p[i];
             }
@@ -36,32 +38,32 @@ namespace OE.ALGA.Optimalizalas
         }
         public bool Ervenyes(bool[] pakolas) => OsszSuly(pakolas) <= Wmax;
     }
-
     public class NyersEro<T>
     {
         int m; //lehetséges megoldások
         Func<int, T> generator; //lehetséges megoldások generálására referencia
         Func<T, float> josag; //lehetséges megoldások értékeinek generlására referencia
-        public int LepesSzam { get; }
+        public int LepesSzam { get; private set; }
         public NyersEro(int m, Func<int, T> generator, Func<T, float> josag)
         {
             this.m = m;
             this.generator = generator;
             this.josag = josag;
+            LepesSzam = 0;
         }
-
         public T OptimalisMegoldas()
         {
-            T o = generator.Invoke(0);
-            for (int i = 1; i < m; i++)
+            LepesSzam = 0;
+            T o = generator.Invoke(1);
+            for (int i = 2; i <= m; i++)
             {
+                LepesSzam++;
                 T megoldas = generator.Invoke(i);
                 if (josag.Invoke(megoldas) > josag.Invoke(o)) o = megoldas;
             }
             return o;
         }
     }
-
     public class NyersEroHatizsakPakolas
     {
         public int LepesSzam { get; private set; }
@@ -69,36 +71,35 @@ namespace OE.ALGA.Optimalizalas
         public NyersEroHatizsakPakolas(HatizsakProblema problema)
         {
             this.problema = problema;
+            LepesSzam = 0;
         }
-
         public bool[] Generator(int i)
         {
-            int szam = i;
+            int szam = i - 1;
             bool[] K = new bool[problema.n];
-            for (int j = 0; j < problema.n; j++)
+            for (int j = 1; j < problema.n; j++)
             {
-                K[j] = (szam / 2 ^ (j - 1)) % 2 == 1;
+                K[j] = (szam / EgeszHatvany(2, (j - 1))) % 2 == 1;
             }
             return K;
         }
         public float Josag(bool[] pakolas) => problema.Ervenyes(pakolas) ? problema.OsszErtek(pakolas) : -1;
         public bool[] OptimalisMegoldas()
         {
-            NyersEro<bool[]> pakolas = new NyersEro<bool[]>(2 ^ problema.n, Generator, Josag);
+            NyersEro<bool[]> pakolas = new NyersEro<bool[]>(EgeszHatvany(2, problema.n), Generator, Josag);
             bool[] optimalis = pakolas.OptimalisMegoldas();
             LepesSzam = pakolas.LepesSzam;
             return optimalis;
         }
-
-        public float OptimalisErtek()
+        public float OptimalisErtek() => problema.OsszErtek(OptimalisMegoldas());
+        static int EgeszHatvany(int alap, int kitevo)
         {
-            bool[] O = Generator(0);
-            for (int i = 1; i < (2^problema.n); i++)
+            int ertek = 1;
+            for (int i = 0; i < kitevo; i++)
             {
-                bool[] K = Generator(i);
-                if (problema.OsszSuly(K) <= problema.Wmax && problema.OsszErtek(K) > problema.OsszErtek(O)) O = K;
+                ertek *= alap;
             }
-            return problema.OsszErtek(O);
+            return ertek;
         }
     }
 }
